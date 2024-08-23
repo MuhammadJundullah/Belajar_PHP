9f66ae8419a6e3f4aed6b75c4a6f2aefbaf10182
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-08-23 21:38:19 +0700

    rebuild

--- Blog Laravel/Blog/vite.config.js
+++ Blog Laravel/Blog/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: "dist", // Pastikan output build di folder 'dist'
+    rollupOptions: {
+      input: {
+        main: "/resources/js/app.js",
+      },
+    },
+  },
 });
