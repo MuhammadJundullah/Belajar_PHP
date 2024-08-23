b6f562fb017e544e8235dfac63dda70b09dbdfc1
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-08-23 20:39:16 +0700

    edit

--- Blog Laravel/Blog/config/app.php
+++ Blog Laravel/Blog/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'laravel'),
+    'name' => env('APP_NAME', 'BLOG'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://Blog.test'),
+    'url' => env('APP_URL', 'http://blog.portofolio.great-site.net'),
 
     /*
     |--------------------------------------------------------------------------
