35b47c1646ef67380886e1270f8260c423b589a6
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-08-28 12:36:46 +0700

    push a compressed files

--- Blog Laravel/Blog/laravel/resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>   
-    <x-slot:title>{{$title}}</x-slot:title>
-    <h3>Selamat datang di {{$title}}</h3>
-    <p>Nama : {{$nama}}</p>
-</x-layout>
-
--- Blog Laravel/Blog/laravel/resources/views/contact.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-  <x-slot:title>{{$title}}</x-slot:title>
-  <h3>Selamat datang di Contact !</h3>
-</x-layout>
